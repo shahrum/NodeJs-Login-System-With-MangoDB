5d31fb11d4ed4c34f260137f1bd1f579344694dd
Shahram Hosseinabadi <shha268@gmail.com>
2018-03-22 19:29:49 +1100

    register

--- loginapp/routes/users.js
+++ loginapp/routes/users.js
@@ -10,5 +10,29 @@ router.get('/login',function(req, res){
 	res.render('login');
 });
 
+// Post Register
+router.post('/register', function(req, res){
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('register',{
+			errors:errors
+		});
+	} else {
+		var newUser = new User({
+			name: name,
+			email:email,
+			username: username,
+			password: password
+		});
+
+	}
+});
+
 
 module.exports = router;
